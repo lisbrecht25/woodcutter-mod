ca97e648993316715df9e1d936b3b3b4c99b6f19
lukeisbrecht <lukeisbrecht25@gmail.com>
2026-01-02 19:28:25 -0500

    Initial commit

---
+++ src/client/java/lukeisbrecht/woodcutter/WoodcutterClient.java
@@ -0,0 +1,10 @@
+package lukeisbrecht.woodcutter;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WoodcutterClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/lukeisbrecht/woodcutter/WoodcutterDataGenerator.java
@@ -0,0 +1,11 @@
+package lukeisbrecht.woodcutter;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class WoodcutterDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
